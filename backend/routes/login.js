eb91441df166f30dbd5c9cb1f9b34734ae3fe561
irynaLv <iryna92lv@gmail.com>
2014-04-02 19:16:12 +0300

    add backend part of app.

---
+++ backend/routes/login.js
@@ -0,0 +1,22 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ikush
+ * Date: 4/1/14
+ * Time: 8:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+   /*
+   *
+   *
+   *
+   * */
+
+/*
+ * Login user
+ */
+
+exports.login = function(req, res){
+    //todo add user login
+
+    res.render('index', { title: 'Express' });
+};

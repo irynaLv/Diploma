24b186925d7506b5b2dee53167752f77d291ef01
amamchur <andrii@mamchur.net>
2014-06-14 14:42:22 +0300

    Migrated project to Sencha Cmd 5.0

--- frontend/app.js
+++ frontend/app.js
@@ -1,66 +1,25 @@
 /*
- This file is generated and updated by Sencha Cmd. You can edit this file as
- needed for your application, but these edits will have to be merged by
- Sencha Cmd when it performs code generation tasks such as generating new
- models, controllers or views and when running "sencha app upgrade".
+    This file is generated and updated by Sencha Cmd. You can edit this file as
+    needed for your application, but these edits will have to be merged by
+    Sencha Cmd when it performs code generation tasks such as generating new
+    models, controllers or views and when running "sencha app upgrade".
 
- Ideally changes to this file would be limited and most work would be done
- in other places (such as Controllers). If Sencha Cmd cannot merge your
- changes and its generated code, it will produce a "merge conflict" that you
- will need to resolve manually.
- */
-
-Ext.Loader.setConfig({
-    disableCaching: false
-
-});
-
-
-Ext.Loader.setPath({
-    'Ext': '../touch/src',
-//    'CoPilot': '../common',
-//    'oAuth': '../common/oAuth',
-    'TL': 'app'
-});
-
+    Ideally changes to this file would be limited and most work would be done
+    in other places (such as Controllers). If Sencha Cmd cannot merge your
+    changes and its generated code, it will produce a "merge conflict" that you
+    will need to resolve manually.
+*/
 
 Ext.application({
     name: 'DL',
-//	autoCreateViewport: true,
 
     requires: [
-        'Ext.Ajax',
         'Ext.MessageBox'
     ],
 
     views: [
-
-        "Main",
-        'Menu',
-        'XTitlebar',
-        'LoginForm',
-        'RegistrationForm',
-        'AddDocument',
-        'Search',
-        'News'
+        'Main'
     ],
-
-    controllers: [
-        // TODO: add controllers here
-
-        'RegistrationForm'
-//            'Main'
-    ],
-
-    stores: [
-        // TODO: add stores here
-//            'DL.store.Users'
-    ],
-
-    models: [
-//            'DL.model.User'
-    ],
-
 
     icon: {
         '57': 'resources/icons/Icon.png',
@@ -68,7 +27,7 @@ Ext.application({
         '114': 'resources/icons/Icon@2x.png',
         '144': 'resources/icons/Icon~ipad@2x.png'
     },
-//
+
     isIconPrecomposed: true,
 
     startupImage: {
@@ -82,26 +41,8 @@ Ext.application({
 
     launch: function() {
         // Destroy the #appLoadingIndicator element
-//        Ext.Ajax.request({
-//            method: 'POST',
-//            url: 'http://localhost:3000/register',
-//            params: {
-//                username: 'test1',
-//                password: 'test1',
-//                firstName:'test1',
-//                secondName: 'test1',
-//                email: 'test1@mail.com',
-//                role: 1,
-//                birthDay: new Date()
-//            },
-//            success: function(response){
-//                var text = response.responseText;
-//                // process server response here
-//            }
-//        });
-
         Ext.fly('appLoadingIndicator').destroy();
-//
+
         // Initialize the main view
         Ext.Viewport.add(Ext.create('DL.view.Main'));
     },

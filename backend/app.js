de70fc4bb5c954752c939b346d05e21a8655e785
amamchur <andrii@mamchur.net>
2014-06-14 18:02:35 +0300

    Recreated backend project

--- backend/app.js
+++ backend/app.js
@@ -1,131 +1,66 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var mongoose = require('mongoose');
-mongoose.set('debug', true);
 var http = require('http');
+var mongoose = require('mongoose');
+var express = require('express');
 var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var users = require('./routes/users');
 
 var app = express();
 
-require('./models/user');
-
-
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var mongoose = require('mongoose');
-var User = mongoose.model('user');
-
-// end of dependencies.
-
-// TODO: добавить done()?
-//passport.use(new LocalStrategy({
-//    usernameField: 'username',
-//    passwordField: 'password'
-//}, function(username, password,done){
-//    User.findOne({ username : username},function(err,user){
-//        return err
-//            ? done(err)
-//            : user
-//            ? password === user.password
-//            ? done(null, user)
-//            : done(null, false, { message: 'Incorrect password.' })
-//            : done(null, false, { message: 'Incorrect username.' });
-//    });
-//}));
-//
-//
-//passport.serializeUser(function(user, done) {
-//    done(null, user.id);
-//});
-//
-//
-//passport.deserializeUser(function(id, done) {
-//    User.findById(id, function(err,user){
-//        err
-//            ? done(err)
-//            : done(null,user);
-//    });
-//});
-
-
-
-
-
-
-var db = mongoose.createConnection('mongodb://localhost:27017/DL');
-//db.on('error', console.error.bind(console, 'connection error:'));
-
-mongoose.connection;
-
-var routes = require('./routes');
-var user = require('./routes/user');
-var login = require('./routes/login');
-
-
-
-//var userSchema = mongoose.Schema({
-//    login: String,
-//    password: String
-//});
-//
-//var User = mongoose.model('User', userSchema);
-
-//mongoose.connect('mongodb://localhost:27017/users');
-// all environments
-app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use('/users', users);
 
-app.use(express.cookieParser());
-app.use(express.bodyParser());
-app.use(express.session({ secret: 'SECRET' }));
-//Passport
-app.use(passport.initialize());
-app.use(passport.session());
+console.log(process.env.PRODUCTION);
 
-app.use(app.router);
+if (process.env.PRODUCTION) {
+    console.log('Production');
+    app.use(express.static(path.join(__dirname, 'public')));
+} else {
+    console.log('Development');
+    app.use(express.static(path.join(__dirname, '..', 'frontend')));
+}
 
-passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password'
-}, function(username, password, done){
-    User.findOne({ username : username},function(err,user){
-        return err
-            ? done(err)
-            : user
-            ? password === user.password
-            ? done(null, user)
-            : done(null, false, { message: 'Incorrect password.' })
-            : done(null, false, { message: 'Incorrect username.' });
-    });
-}));
-
-
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
+/// error handlers
 
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err,user){
-        err
-            ? done(err)
-            : done(null,user);
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
     });
 });
-
-
 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -133,18 +68,5 @@ app.all('*', function(req, res, next) {
     next();
 });
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-//Authentication functionality
-app.post('/login', login.login);
-app.post('/register', login.register);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+var port = port = process.env.PORT || 5000;
+app.listen(port);

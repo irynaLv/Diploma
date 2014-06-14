20072183ef45f767cc5b622f1181d43699bc9011
irynaLv <iryna92lv@gmail.com>
2014-06-14 14:08:10 +0300

    Change style in all panel. Production build

--- frontend/app.js
+++ frontend/app.js
@@ -58,7 +58,7 @@ Ext.application({
     ],
 
     models: [
-//            'DL.model.User'
+
     ],
 
 
--- frontend/app/view/AddDocument.js
+++ frontend/app/view/AddDocument.js
@@ -9,151 +9,157 @@ Ext.define('DL.view.AddDocument', {
     config: {
         layout: {
             type: 'vbox',
-            align: 'left'
+            align:'stretch'
         },
-        modal: true,
+        scrollable: {
+            direction: 'vertical',
+            directionLock: true
+        },
+//        modal: true,
+        maxWidth: '18em',
+        maxHeight: '22em',
+        width: '90%',
+        height: '80%',
         flex: 1,
-        hideOnMaskTap:true,
+//        hideOnMaskTap:true,
         cls: 'add-document-panel',
         items: [
-//            {
-//                xtype: 'segmentedbutton',
-//                cls: 'choose-segmented-btn',
-//                width:'100%',
-//                items: [
-//                    {
-//                        text: 'Документи',
-//                        width:'50%',
-//                        cls: 'documents',
-//                        pressed:true
-//                    },
-//                    {
-//                        text: 'Оголошення',
-//                        width:'50%',
-//                        cls: 'advert'
-//                    }
-//
-//                ]
-//
-//            },
-            {
-                xtype: 'textfield',
-                label:'Назва',
-                labelWidth:'30%',
-                width: '100%',
-                cls:'name',
-                itemId: 'name'
-            },
-            {
-                xtype: 'textareafield',
-                label:'Опис',
-                labelWidth:'30%',
-                width: '100%',
-                itemId: 'description',
-                cls:'description'
-            },
-            {
-                xtype: 'selectfield',
-                width: '100%',
-                label: 'Тип',
-                labelWidth:'30%',
-                cls:'type',
-                width: '100%',
-                itemId: 'type',
-                usePicker: true,
-                options: [
-                    {text: 'Навчальні матеріали',  value: 1},
-                    {text: 'Нормативні документи', value: 2},
-                    {text: 'Протоколи засідань',  value: 3},
-                    {text: 'Інформаційні матеріали',  value: 4},
-                    {text: 'Оголошення',  value: 5},
-                    {text: 'Інше',  value: 6}
-                ]
-            },
-            {
-                xtype:'container',
-                width: '100%',
-                cls:'role',
-                itemId:'role',
-                layout:'hbox',
-                items: [
                     {
+                        xtype: 'textfield',
+                        label:'Назва',
+                        labelWidth:'30%',
+                        width: '100%',
+                        cls:'name',
+                        itemId: 'name'
+                    },
+                    {
+                        xtype: 'textareafield',
+                        label:'Опис',
+                        labelWidth:'30%',
+                        width: '100%',
+                        itemId: 'description',
+                        cls:'description'
+                    },
+                    {
+                        xtype: 'selectfield',
+                        width: '100%',
+                        label: 'Тип',
+                        labelWidth:'30%',
+                        cls:'type',
+                        width: '100%',
+                        itemId: 'type',
+                        usePicker: true,
+                        options: [
+                            {text: 'Навчальні матеріали',  value: 1},
+                            {text: 'Нормативні документи', value: 2},
+                            {text: 'Протоколи засідань',  value: 3},
+                            {text: 'Інформаційні матеріали',  value: 4},
+                            {text: 'Оголошення',  value: 5},
+                            {text: 'Інше',  value: 6}
+                        ]
+                    },
+                    {
+                        xtype:'container',
+                        width: '100%',
+                        cls:'role',
+                        itemId:'role',
+                        layout:'hbox',
+                        items: [
+                            {
 
-                        xtype: 'container',
-                        html:'Рівень досупу',
-                        cls: 'title'
+                                xtype: 'container',
+                                html:'Рівень доступу',
+                                cls: 'title',
+                                width: '30%'
+                            },
+                            {
+                                xtype: 'container',
+                                width: '70%',
+                                items: [
+                                    {
+                                        xtype: 'checkboxfield',
+                                        labelWidth: '70%',
+                                        itemId: 'worker',
+                                        label: 'Працівники'
+                                    },
+                                    {
+                                        xtype: 'checkboxfield',
+                                        labelWidth: '70%',
+                                        itemId: 'student',
+                                        label: 'Студенти'
+
+                                    },
+                                    {
+                                        xtype: 'checkboxfield',
+                                        labelWidth: '70%',
+                                        label: 'Власник',
+                                        itemId: 'owner'
+                                    },
+                                    {
+                                        xtype: 'checkboxfield',
+                                        labelWidth: '70%',
+                                        label: 'Всі',
+                                        itemId: 'allUsers'
+                                    }
+
+                                ]
+                            }
+
+                        ]
+                    },
+                    {
+                        xtype: 'filefield',
+                        label: "Файл:",
+                        cls: 'file',
+                        itemId: 'file',
+                        buttonText: 'Виберіть файл',
+//                        buttonWidth: '3em',
+                        msgTarget: 'side',
+                        allowBlank: false,
+                        anchor: '100%'
+//                hidden:true
+
                     },
                     {
                         xtype: 'container',
-                        width: '70%',
-                        items: [
+                        layout: 'hbox',
+                        cls: 'btn-container',
+                        items:[
                             {
-                                xtype: 'checkboxfield',
-                                labelWidth: '70%',
-                                itemId: 'worker',
-                                label: 'Працівники'
+                                xtype: 'button',
+                                buttonText: 'Файл',
+                                text: 'Завантажити',
+                                cls: 'upload-btn',
+                                itemId: 'upload-btn',
+                                width:'35%'
                             },
                             {
-                                xtype: 'checkboxfield',
-                                labelWidth: '70%',
-                                itemId: 'student',
-                                label: 'Студенти'
-
+                                xtype: 'spacer',
+                                width: '10%'
                             },
                             {
-                                xtype: 'checkboxfield',
-                                labelWidth: '70%',
-                                label: 'Власник',
-                                itemId: 'owner'
-                            },
-                            {
-                                xtype: 'checkboxfield',
-                                labelWidth: '70%',
-                                label: 'Всі',
-                                itemId: 'allUsers'
+                                xtype: 'segmentedbutton',
+                                width:'55%',
+                                items: [
+                                    {
+                                        xtype: 'button',
+                                        text: 'Очистити дані',
+                                        cls: 'clear-btn',
+                                        itemId: 'clear-btn',
+                                        width:'50%'
+                                    },
+                                    {
+                                        xtype: 'button',
+                                        text: 'Закрити',
+                                        cls: 'close-btn',
+                                        itemId: 'close-btn',
+                                        width:'50%'
+                                    }
+                                ]
                             }
 
                         ]
                     }
-
-                ]
-            },
-            {
-                xtype: 'filefield',
-                label: "Файл:",
-                cls: 'file',
-                itemId: 'file',
-                buttonText: 'Виберіть файл',
-                msgTarget: 'side',
-                allowBlank: false,
-                anchor: '100%'
-//                hidden:true
-
-            },
-            {
-                xtype: 'container',
-                layout: 'hbox',
-                items:[
-                    {
-                        xtype: 'button',
-                        buttonText: 'Файл',
-                        text: 'Завантажити',
-                        cls: 'upload-btn',
-                        itemId: 'upload-btn'
-                    },
-                    {
-                        xtype: 'spacer',
-                        width: '8em'
-                    },
-                    {
-                        xtype: 'button',
-                        text: 'Очистити дані',
-                        cls: 'clear-btn',
-                        itemId: 'clear-btn'
-//                        width:'30%'
-                    }
-                ]
-            }
 
         ]
     },
@@ -173,6 +179,8 @@ Ext.define('DL.view.AddDocument', {
 
         this.type.on('change', this.setFileHidden, this);
         this.clearBtn.on('tap', this.clearPanel, this);
+        this.uploadBtn.on('tap', this.onUploadBtn, this)
+
         this.callParent();
 
     },
@@ -191,6 +199,9 @@ Ext.define('DL.view.AddDocument', {
         this.owner.setChecked(false);
         this.allUserAcces.setChecked(false);
         this.file.element.down('input').dom.value = '';
+
+    },
+    onUploadBtn: function(){
 
     }
 })
